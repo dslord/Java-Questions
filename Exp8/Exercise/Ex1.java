67d7091a36eee3a2a6c03eb0353c185bb3c92c27
dslord <devanshsingh3107@gmail.com>
2026-07-15 19:54:31 +0530

    Performed Assignment 08 Exercises

---
+++ Exp8/Exercise/Ex1.java
@@ -0,0 +1,22 @@
+// Create an Employee and Manager program where Manager accesses Employee details using super keyword.
+package Exp8.Exercise;
+
+class Employee {
+    void display() {
+        System.out.println("This is employee class");
+    }
+}
+
+class Manager extends Employee {
+    void display() {
+        super.display();
+        System.out.println("This is manager class");
+    }
+}
+
+public class Ex1 {
+    public static void main (String[] args) {
+        Manager m = new Manager();
+        m.display();
+    }
+}
---
+++ Exp8/Exercise/Ex2.java
@@ -0,0 +1,30 @@
+// Develop a Vehicle Insurance System where child insurance classes access parent vehicle information using the super keyword.
+package Exp8.Exercise;
+
+class Vehicle {
+    String brand;
+    String model;
+
+    Vehicle(String b, String m) {
+        this.brand = b;
+        this.model = m;
+    }
+}
+
+class Insurance extends Vehicle {
+    Insurance(String b, String m) {
+        super(b, m);
+    }
+
+    void display() {
+        System.out.println("Brand : " + brand);
+        System.out.println("Model : " + model);
+    }
+}
+
+public class Ex2 {
+    public static void main (String[] args) {
+        Insurance i = new Insurance("Toyota", "Etios");
+        i.display();
+    }
+}
