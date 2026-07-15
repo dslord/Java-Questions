51aa5ce4aab36288e4497ecbca2441f0b0043baa
dslord <devanshsingh3107@gmail.com>
2026-07-15 19:54:19 +0530

    Performed Assignment 08

---
+++ Exp8/Main.java
@@ -0,0 +1,21 @@
+package Exp8;
+
+class Parent {
+    void display () {
+        System.out.println("Parent class method");
+    }
+}
+
+class Child extends Parent {
+    void display() {
+        super.display();
+        System.out.println("Child class method");
+    }
+}
+
+public class Main {
+    public static void main (String[] args) {
+        Child c = new Child();
+        c.display();
+    }
+}
